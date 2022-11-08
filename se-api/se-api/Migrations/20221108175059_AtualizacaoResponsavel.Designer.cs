c2670eb81bedd34cf8311430f2a6fa8456113ffe
Marlon Vinicius Batista <marlon.batista18@hotmail.com>
2022-11-08 19:23:04 -0300

    Modificado Migrations

--- se-api/se-api/Migrations/20221108175059_AtualizacaoResponsavel.Designer.cs
+++ se-api/se-api/Migrations/20221108175059_AtualizacaoResponsavel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using se_api.Context;
 
@@ -11,9 +12,10 @@ using se_api.Context;
 namespace se_api.Migrations
 {
     [DbContext(typeof(SEContext))]
-    partial class SEContextModelSnapshot : ModelSnapshot
+    [Migration("20221108175059_AtualizacaoResponsavel")]
+    partial class AtualizacaoResponsavel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
